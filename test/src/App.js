00717c45bf003f164b621d5d31b805ab0ea98d91
Sunny <maryrosedvillacampa@gmail.com>
2023-06-21 12:52:50 +0800

    commit

--- test/src/App.js
+++ test/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.js';
 import Cube from './components/Cube.jsx'
 
 function App() {
--- test/src/Pages/Home.jsx
+++ test/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Cube from '../components/Cube.jsx'
 
 function Home() {
     return(
         <>
         <div>
             <p>Hello there!</p>
+            <Cube />
         </div>
         </>
     )
