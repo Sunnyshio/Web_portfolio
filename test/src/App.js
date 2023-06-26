9e0e4abb3e8e44951e36445b8f601f7153f0e634
Sunny <maryrosedvillacampa@gmail.com>
2023-06-26 11:04:37 +0800

    commit

--- test/src/App.js
+++ test/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Nav from './components/Navbar.jsx'
-import Cube from './components/Cube.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Content from './components/Content.jsx'
 
